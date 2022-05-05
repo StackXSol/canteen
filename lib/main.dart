5f6031bce54c734e5d791827eddec13991e66836
Vaibhav Chauhan <68010116+vaibhavC1302@users.noreply.github.com>
2022-05-05 16:37:52 +0530

    added cart and orderdetails page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:canteen/screens/cart.dart';
 import 'package:canteen/screens/homepage.dart';
 import 'package:canteen/screens/loading.dart';
+import 'package:canteen/screens/order_details.dart';
+import 'package:canteen/screens/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,17 +14,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: OrderDetails(),
       title: 'Canteen',
       theme: ThemeData(
         scaffoldBackgroundColor: Color.fromARGB(255, 212, 212, 212),
         fontFamily: "Gilroy",
         textTheme: TextTheme(bodyText2: TextStyle(color: Colors.black)),
       ),
-      routes: {
-        '/homepage': (context) => const HomePage(),
-        '/loadingscreen': (context) => const LoadingPage(),
-      },
-      initialRoute: '/loadingscreen',
+      // routes: {
+      //   '/homepage': (context) => const HomePage(),
+      //   '/loadingscreen': (context) => const LoadingPage(),
+      // },
+      // initialRoute: '/loadingscreen',
     );
   }
 }
