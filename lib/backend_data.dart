3e475e15a90399706a69e55e2778b3f8fb1b4a77
Vaibhav Chauhan <68010116+vaibhavC1302@users.noreply.github.com>
2022-05-08 16:28:11 +0530

    added cart and orderdetails page

---
+++ lib/backend_data.dart
@@ -0,0 +1,16 @@
+class BackendData {
+  String full_name;
+  String email;
+  int phone;
+  String uid;
+  // String College;
+  int Roll_no;
+  BackendData({
+    required this.full_name,
+    required this.email,
+    required this.phone,
+    required this.uid,
+    // required this.College,
+    required this.Roll_no,
+  });
+}
