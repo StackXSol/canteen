5ec44334e72143c5e3fe3f00000a56080152ae65
Vaibhav Chauhan <68010116+vaibhavC1302@users.noreply.github.com>
2022-05-21 02:52:45 +0530

    email verification screen ui and timer

--- lib/screens/email_verify_screen.dart
+++ lib/screens/email_verify_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:canteen/screens/homepage.dart';
+import 'package:canteen/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,13 +17,33 @@ class EmailverificationScreen extends StatefulWidget {
 
 class _EmailverificationScreenState extends State<EmailverificationScreen> {
   bool isemailverified = false;
+  bool wait = true;
+  int start = 30;
 
   Timer? timer;
+
+  void starttimer() {
+    const onsec = Duration(seconds: 1);
+    Timer _timer = Timer.periodic(onsec, (_timer) {
+      if (start == 0) {
+        setState(() {
+          wait = false;
+          _timer.cancel();
+        });
+      } else {
+        setState(() {
+          start--;
+        });
+      }
+    });
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    starttimer();
 
     isemailverified = FirebaseAuth.instance.currentUser!.emailVerified;
 
@@ -49,9 +71,62 @@ class _EmailverificationScreenState extends State<EmailverificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          alignment: Alignment.center,
-          child: Text("email verification screen")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "Haven't recieved email? Send again in ",
+                  style: TextStyle(color: Colors.black)),
+              TextSpan(text: "00:$start ", style: TextStyle(color: orange_color)),
+                  TextSpan(
+                      text: "sec",
+                      style: TextStyle(color: Colors.black)),
+            ])),
+            SizedBox(
+              height: getheight(context, 30),
+            ),
+            wait
+                ? Container(
+                    alignment: Alignment.center,
+                    height: 50,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey.shade800,
+                    ),
+                    child: Text(
+                      "Resend",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                : InkWell(
+                    onTap: () {
+                      FirebaseAuth.instance.currentUser!.sendEmailVerification;
+                      starttimer();
+                      setState(() {
+                        wait = true;
+                        start = 30;
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: 120,
+                      decoration: BoxDecoration(
+                          color: orange_color,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text(
+                        "Resend",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+          ],
+        ),
+      ),
     );
   }
 }
